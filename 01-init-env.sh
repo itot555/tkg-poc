e94b7d2b1ceb74ffa6b0c7fa456f9accb7978997
Yogendra Rampuria (Yogi) <yogendrarampuria@gmail.com>
2020-10-18 16:02:32 +0800

    Adding more vars to init

--- 01-init-env.sh
+++ 01-init-env.sh
@@ -1,19 +1,27 @@
 #!/usr/bin/env bash
 SCRIPT_ROOT=$( cd `dirname $0`; pwd)
 echo "Read description in the $SCRIPT_ROOT/README.md"
-read -p "Input [CLIENT]: " CLIENT
-read -p "Input [JUMPBOX_IP]: " JUMPBOX_IP
-read -p "Input [POC_DOMAIN]: " POC_DOMAIN
+
+read -p " Enter value for client name [CLIENT]: " CLIENT
+read -p " Enter value for jumpbox ip [JUMPBOX_IP]: " JUMPBOX_IP
+read -p " Enter value for poc domain [POC_DOMAIN]: " POC_DOMAIN
+read -p " Enter value for root domain [ROOT_DOMAIN]: " ROOT_DOMAIN
+read -p " Enter value for vcenter URL [GOVC_URL]: " GOVC_URL
+read -p " Enter value for vcenter username [GOVC_USERNAME]: " GOVC_USERNAME
+read -p " Enter value for vcenter password [GOCV_PASSWORD]: " GOCV_PASSWORD
 
 cat <<EOF  > $SCRIPT_ROOT/.env
 CLIENT=$CLIENT
 JUMPBOX_IP=$JUMPBOX_IP
 POC_DOMAIN=$POC_DOMAIN
-ROOT_DOMAIN=${CLIENT}.${POC_DOMAIN}
+ROOT_DOMAIN=$ROOT_DOMAIN
 LOCAL_REGISTRY=$JUMPBOX_IP:5000
 TKG_CUSTOM_IMAGE_REPOSITORY=$JUMPBOX_IP:5000
 TKG_CUSTOM_IMAGE_REPOSITORY_SKIP_TLS_VERIFY=true
-
+GOVC_URL=$GOVC_URL
+GOVC_INSECURE=true
+GOVC_USERNAME=$GOVC_USERNAME
+GOCV_PASSWORD=$GOCV_PASSWORD
 EOF
 
 
