8dcd24f307a3716cf35aaf91f2bf89558d7a52ce
Yogendra Rampuria (Yogi) <yogendrarampuria@gmail.com>
2020-10-18 20:12:03 +0800

    moved

--- 06-tkg-create-guest.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_ROOT=$( cd `dirname $0`; pwd)
-source $SCRIPT_ROOT/.env
-
-GUEST_NAME=${1?"guest cluster name required"}; shift
-GUEST_VIP=${1?"guest cluster vip required"};shift
-tkg create cluster $GUEST_NAME -p prod -k v1.19.1+vmware.2   --vsphere-controlplane-endpoint-ip  $GUEST_VIP -c 1 -w 3
